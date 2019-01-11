5e0fc194e12a585f501f2c68774d0bf7879ccfd1
Jason Velez <jvelez20@yahoo.com>
2019-01-11 15:43:53 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -20,7 +20,7 @@ describe('functions', ()=>{
   })
 
   describe('holidayCountdown(holiday, days)', () => {
-    it('returns "It\'s ${days} days until ${holiday}!"', () => {
+    console.log('returns "It\'s ${days} days until ${holiday}!"', () => {
       expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
     })
   })
