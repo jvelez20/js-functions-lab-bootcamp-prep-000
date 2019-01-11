27cd71c0c983d7bafa4c56b3f873f9dc1365ec63
Jason Velez <jvelez20@yahoo.com>
2019-01-11 15:31:14 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,7 @@
 
 describe('functions', () => {
   describe('happyHolidays', () => {
-    it(returns "Happy holidays!"', () => {
+    it(returns"Happy holidays!"', () => {
       expect(happyHolidays()).toEqual('Happy holidays!')
     })
   })
